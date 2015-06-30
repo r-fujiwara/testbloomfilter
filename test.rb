72007504f8c9be0c821f614bcf25677a5d354e69
r-fujiwara <r-fujiwara@nekojarashi.com>
2015-06-30 11:06:29 +0900

    modify stats

--- test.rb
+++ test.rb
@@ -10,10 +10,10 @@ def gen_random_word(list, method, index)
   rand_word = Digest::SHA256.hexdigest(index.to_s)
   prev_length = list.length
   list << rand_word
-  current_length = list.uniq!.length
+  current_length = list.uniq.length
   if prev_length == current_length
     puts "********duplicate!...#{rand_word}***********"
-    gen_random_word(list)
+    gen_random_word(list.uniq!)
   else
     puts "#{method}...#{rand_word}"
     $bf.send method.to_sym, rand_word
@@ -23,7 +23,7 @@ end
 def set_random_unique_words(size, method)
   list = []
   index_list = (1..size).to_a
-  Parallel.each(index_list, in_threads: 8) do |i|
+  Parallel.each(index_list, in_threads: 16) do |i|
   #size.times do |i|
     puts "index...#{i}"
     gen_random_word(list, method, i)
@@ -31,6 +31,7 @@ def set_random_unique_words(size, method)
 end
 
 set_random_unique_words FILTER_SIZE, 'insert'
+
 set_random_unique_words FILTER_SIZE, 'include?'
 
-bf.stats
+$bf.stats
