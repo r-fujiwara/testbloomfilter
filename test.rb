3e9e21d9e8d7d731ffacfa17898db91acd16253f
r-fujiwara <r-fujiwara@nekojarashi.com>
2015-06-30 12:05:51 +0900

    modify logic

--- test.rb
+++ test.rb
@@ -2,36 +2,46 @@ require 'bundler'
 Bundler.require
 require 'digest'
 
-FILTER_SIZE = ARGV.first.to_i rescue 150000
+GEN_SIZE = ARGV.first.to_i rescue 150000
+BITS_FILTER = 191702
 
-$bf = BloomFilter::Native.new(:size => FILTER_SIZE, :hashes => 13, :raise => false)
+$bf = BloomFilter::Native.new(:size => BITS_FILTER, :hashes => 13, :raise => false)
 
-def gen_random_word(list, method, index)
+def gen_random_word(list, index)
   rand_word = Digest::SHA256.hexdigest(index.to_s)
   prev_length = list.length
   list << rand_word
   current_length = list.uniq.length
   if prev_length == current_length
-    puts "********duplicate!...#{rand_word}***********"
     gen_random_word(list.uniq!)
-  else
-    puts "#{method}...#{rand_word}"
-    $bf.send method.to_sym, rand_word
   end
 end
 
-def set_random_unique_words(size, method)
+def random_unique_words(size)
   list = []
   index_list = (1..size).to_a
   Parallel.each(index_list, in_threads: 16) do |i|
-  #size.times do |i|
-    puts "index...#{i}"
-    gen_random_word(list, method, i)
+    gen_random_word(list, i)
+  end
+  list
+end
+
+def affect_filter(list, method)
+  Parallel.each(list, in_threads: 16) do |w|
+    $bf.send method.to_sym, w
   end
 end
 
-set_random_unique_words FILTER_SIZE, 'insert'
+unique_words = random_unique_words GEN_SIZE
 
-set_random_unique_words FILTER_SIZE, 'include?'
+puts "unique_words size....#{unique_words.length}"
+
+affect_filter(unique_words, "insert")
+
+other_words = random_unique_words GEN_SIZE
+
+puts "counter unique words size...#{other_words.length}"
+
+affect_filter(unique_words, "include?")
 
 $bf.stats
