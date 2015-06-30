20ecc2d99218a12e07b7eb59f8db0c9f884410e0
r-fujiwara <r-fujiwara@nekojarashi.com>
2015-06-30 13:14:47 +0900

    update

--- test.rb
+++ test.rb
@@ -7,41 +7,52 @@ BITS_FILTER = 191702
 
 $bf = BloomFilter::Native.new(:size => BITS_FILTER, :hashes => 13, :raise => false)
 
-def gen_random_word(list, index)
+def gen_random_word(hash, index)
   rand_word = Digest::SHA256.hexdigest(index.to_s)
-  prev_length = list.length
-  list << rand_word
-  current_length = list.uniq.length
-  if prev_length == current_length
-    gen_random_word(list.uniq!)
+  if hash.key? rand_word
+    gen_random_word(hash, index)
+  else
+    hash[rand_word] = nil
   end
 end
 
-def random_unique_words(size)
-  list = []
-  index_list = (1..size).to_a
-  Parallel.each(index_list, in_threads: 16) do |i|
-    gen_random_word(list, i)
+def random_unique_words(size, offset:)
+  hash = {}
+  size.times do |i|
+    ii = i + offset
+    gen_random_word(hash, ii)
   end
-  list
+  hash
 end
 
 def affect_filter(list, method)
-  Parallel.each(list, in_threads: 16) do |w|
+  list.each do |w|
     $bf.send method.to_sym, w
   end
 end
 
-unique_words = random_unique_words GEN_SIZE
+unique_words = random_unique_words GEN_SIZE, offset: 0
 
 puts "unique_words size....#{unique_words.length}"
 
-affect_filter(unique_words, "insert")
+affect_filter(unique_words.keys, "insert")
 
-other_words = random_unique_words GEN_SIZE
+other_words = random_unique_words GEN_SIZE, offset: GEN_SIZE
 
 puts "counter unique words size...#{other_words.length}"
 
-affect_filter(unique_words, "include?")
+num = 0
+other_words.each do |k, v|
+  if $bf.include? k
+    if unique_words.key? k
+      puts "include...#{k}"
+    else
+      num += 1
+      puts "false positive..#{k}"
+    end
+  end
+end
 
+puts "false positive number ....#{num}"
+sleep 100
 $bf.stats
